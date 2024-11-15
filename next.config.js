0828c7140aa86149495e2ea9c9d8a1abf82aeacf
benf1 <42039229+benf1@users.noreply.github.com>
2024-11-15 03:37:35 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,14 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   async headers() {
     return [
       {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "https://www.figma.com" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
           { key: "Access-Control-Allow-Methods", value: "GET,POST,OPTIONS" },
-          { key: "Access-Control-Allow-Headers", value: "Content-Type, Accept" },
+          { key: "Access-Control-Allow-Headers", value: "Content-Type, Accept, Origin" },
         ]
       }
     ]
